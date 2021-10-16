74c3020f2bf0c544f8658490e32aaddfc56547e8
CezzMartins <cezzmartins@gmail.com>
2021-10-16 14:00:16 -0300

    feat: Add Restart Game

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,7 @@
           @select-card="flipCard"/> 
   </section>
   <h2>{{ status }}</h2>
-  <button @click="shuffleCards">Shuffle</button>
+  <button @click="restartGame">Restart Game</button>
 </template>
 
 <script>
@@ -45,10 +45,23 @@ export default {
       cardList.value = _.shuffle(cardList.value)
     }
 
+    const restartGame = () => {
+      shuffleCards()
+
+      cardList.value = cardList.value.map((card, index) => {
+        return{
+          ...card,
+          matched: false,
+          position: index,
+          visible: false
+        }
+      })
+    }
+
     for (let i = 0; i < 16; i++){
       cardList.value.push({
-        value: i,
-        visible: true,
+        value: 8,
+        visible: false,
         position: i,
         matched: false,
       })
@@ -70,11 +83,9 @@ export default {
         const cardTwo =  currentValue[1]
 
         if(cardOne.faceValue === cardTwo.faceValue){
-          status.value = "Matched!"
           cardList.value[cardOne.position].matched = true
           cardList.value[cardTwo.position].matched = true
         }else {
-          status.value = "Mismatch!"
           cardList.value[cardOne.position].visible = false
           cardList.value[cardTwo.position].visible = false
         }
@@ -82,7 +93,7 @@ export default {
       }
     }, {  deep: true })
     return{
-      cardList, flipCard, userSelection,status,shuffleCards
+      cardList, flipCard, userSelection,status,shuffleCards, restartGame
     }
   }
 }
